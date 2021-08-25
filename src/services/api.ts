1ff43126420e92620dee5b1642fdeb44b5d2035c
Talisson Oliveira <49914130+xdenkai@users.noreply.github.com>
2021-08-25 13:48:48 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
-    api_key: '10ebb4490f753d316a08335c7651b407',
+    api_key: '',
   },
 });
 
